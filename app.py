74dc603a9600c43a0475448c2b47c3c430e9ef00
111kannan <jaikannan111@gmail.com>
2024-05-24 19:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import regex as re
+import re
 import pandas as pd
 from urllib.parse import unquote
 
